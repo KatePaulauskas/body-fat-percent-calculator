57dfd2807aa9ea5459e26d7338096ce12553e3e5
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-02-10 18:32:19 +0000

    Define function to request the user to input skinfold measurements. Provide instructions on how to take measurements.

--- run.py
+++ run.py
@@ -19,9 +19,10 @@ def get_measurements_date():
     Request the user to input the date, when measurements were taken
     """
 
-    print('Welcome to Body Fat Percent Calculator')
+    print('Welcome to Body Fat Percent Calculator\n')
+    print('In order to use the Calculator, please use a skinfold caliper\n')
     while True:
-        print('Enter the date measurements were taken in the following format: DD/MM/YYYY')
+        print('Enter the date measurements were taken in the following format: DD/MM/YYYY.\n')
 
         date_measurements_taken = input('Enter date here:\n')
 
@@ -47,7 +48,7 @@ def validate_date(value):
         return True
 
     except ValueError:
-        print("Incorrect date format. Try again")
+        print("Incorrect date format. Try again\n")
         return False
 
 
@@ -59,7 +60,7 @@ def get_user_name():
     """
 
     while True:
-        print('Enter your name. You can use letters, hyphens, apostrophes, and spaces (where appropriate)')
+        print('Enter your name. You can use letters, hyphens, apostrophes, and spaces (where appropriate)\n')
 
         user_name = input('Enter your name here:\n')
 
@@ -81,7 +82,7 @@ def validate_user_name(name):
     if re.match(pattern, name):
         return True
     else:
-        print("Invalid name. Please ensure it contains only letters, hyphens, apostrophes, and spaces (where appropriate).")
+        print("Invalid name. Please ensure it contains only letters, hyphens, apostrophes, and spaces (where appropriate).\n")
         return False
 
 user_name = get_user_name()
@@ -91,7 +92,7 @@ def get_user_gender():
     Request the user to input their gender
     """
 
-    print('Enter your gender in the followign format: M or F')
+    print('Enter your gender in the followign format: M or F.\n')
 
     user_gender = input('Enter your gender:\n')
 
@@ -99,7 +100,7 @@ def get_user_gender():
         print('Gender is valid!\n')
         return user_gender
     else:
-        print('Invalid input. Please enter M or F')
+        print('Invalid input. Please enter M or F.\n')
         return get_user_gender()
 
 user_gender = get_user_gender()
@@ -110,8 +111,8 @@ def get_user_weight():
     Ensures that the input is a positive number, which can be a float.
     """
     while True:
-        print('Enter your weight in the followign format: 80.5')
-        user_weight = input("Enter your weight here (in kilograms): ")
+        print('Enter your weight in the followign format: 80.5.\n')
+        user_weight = input("Enter your weight here (in kilograms):\n")
 
         try:
             weight = float(user_weight)
@@ -119,8 +120,35 @@ def get_user_weight():
                 print("Weight is valid!\n")
                 return weight
             else:
-                print("Weight must be a positive number. Please enter a valid weight.")
+                print("Weight must be a positive number. Please enter a valid weight.\n")
         except ValueError:
-            print("Invalid input. Please enter a numeric value for weight.")
+            print("Invalid input. Please enter a numeric value for weight.\n")
 
 user_weight = get_user_weight()
+
+def get_skinfold_measurements():
+    """
+    Provide instructions on how the measurements should be taken
+    Request the user to input the skinfold measurements.
+    """
+    
+    print('Equipment: Skinfold caliper.\n')
+    print('Procedure:')
+    print('Measurements are taken on the right side of body. Caliber needs to be perpendicular to the site analyzed.')
+    print('The participant must relax the muscle group that is being assessed.')
+    print('When skin fold is pinched, the practitioner should be taking reading at the middle of the pinched skin, not apex or base.')
+    print('Wait 1 to 2 seconds after releasing caliber, record closest 0.5mm. Retake each site in order to obtain accurate readings.\n')
+    print('Insturctions:\n')
+    print('Tricep: vertical fold at the midpoint of the posterior side of tricep between shoulder and elbow with arm relaxed at the side.\n')
+    print('Chest: diagonal fold half the distance between anterior axillary line and the nipple.\n')
+    print ('Subscapular: diagonal fold 2cm from inferior angle of the scapula.\n')
+    print('Midaxillary: at midaxillary line horizontal to xiphoid process of the sternum.\n')
+    print('Suprailiac: diagonal fold parallel and superior to the iliac crest.\n')
+    print('Abdominal: vertical fold 2cm to the right of the navel.\n')
+    print ('Thigh: midpoint of the anterior side of the upper leg between the patella and top of thigh.\n')
+
+    print('Enter skinfold measurements in mm in the following order: tricep, chest, subscapular, midaxillary, abdominal, suprailiac, thigh.')
+    print('Data should be 7 numbers, separated by commas. Example: 10,5,12,11,25,20,33\n')
+
+    measurements_str = input("Enter your measurements here:\n")
+get_skinfold_measurements()
