dfdc341e4d93889e22df289282587f2aa0c0308f
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-02-10 13:28:40 +0000

    Define the function to request the user to enter the date when the measurements were taken.

--- run.py
+++ run.py
@@ -10,11 +10,13 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('body-fat-persent-calculator')
+SHEET = GSPREAD_CLIENT.open('body-fat-percent-calculator')
 
+def get_todays_date():
+    """
+    Request the user to input the date, when measurements were taken
+    """
+    print("Welcome to Body Fat Percent Calculator")
+    print("Enter the date measurements were taken in the following format DD/MM/YYYY")
 
-measurements = SHEET.worksheet('measurements')
-
-data = measurements.get_all_values()
-
-print(data)
+    date_measurements_taken = input("Enter date here:\n")
